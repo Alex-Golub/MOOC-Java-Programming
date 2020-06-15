e5b14c71acb8aab87dbdd279a8c41f236305dc58
Alex Golub <55350522+Alex-Golub@users.noreply.github.com>
2020-06-15 23:06:17 +0300

    Week 10. Part 1: Handling collections as streams

---
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class ReadingFilesPerLine {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+  }
+
+  public static List<String> read(String file) {
+    try {
+      return Files.lines(Paths.get(file)).collect(Collectors.toCollection(ArrayList::new));
+    } catch (IOException ex) {
+      Logger.getLogger(ReadingFilesPerLine.class.getName()).log(Level.SEVERE, null, ex);
+    }
+
+    return Collections.emptyList();
+  }
+
+}
